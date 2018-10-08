8a8f9ba6c95f0a6558fee80ecfe86978b3a107da
D7701 <D7701@DESKTOP-ODBNK9S>
2018-10-08 14:36:57 +0900

    exSetterDI

---
+++ 201412368-exSetterDI/src/ClassA.java
@@ -0,0 +1,6 @@
+
+public class ClassA {
+	public String methodA() {
+		return "ClassA~!";
+	}
+}
---
+++ 201412368-exSetterDI/src/TestSetterDI.java
@@ -0,0 +1,12 @@
+
+public class TestSetterDI {
+
+ public static void main(String[] args) {
+	 ClassA classA = new ClassA();
+	 ClassB classB = new ClassB();
+	 //Setter based Dependency Injection
+	 classB.setClassA(classA);
+	 classB.methodB();
+ 	}
+
+}
