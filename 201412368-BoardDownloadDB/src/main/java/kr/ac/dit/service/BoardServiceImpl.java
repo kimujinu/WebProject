de1fa9ed0f2d757db008787ffe382c82a08b3b2f
D7701 <D7701@DESKTOP-ODBNK9S>
2018-11-22 15:34:22 +0900

    boardDownloadDB

---
+++ 201412368-BoardDownloadDB/src/main/java/kr/ac/dit/service/BoardServiceImpl.java
@@ -0,0 +1,40 @@
+package kr.ac.dit.service;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import kr.ac.dit.domain.BoardVO;
+import kr.ac.dit.domain.UploadFileVO;
+import kr.ac.dit.persistence.BoardDAO;
+@Service
+public class BoardServiceImpl implements BoardService { //service =  DAO호출 
+	@Autowired
+	private BoardDAO boardDAO;
+	public List<BoardVO> listArticle() throws Exception {
+		return boardDAO.selectList();
+	}
+	public void createArticle(BoardVO boardVO) throws Exception {
+		boardDAO.insert(boardVO);
+	}
+	public BoardVO readArticle(int no) throws Exception {
+		return boardDAO.select(no);
+	}
+	public void updateArticle(BoardVO boardVO) throws Exception {
+		boardDAO.update(boardVO);
+	}
+	public void deleteArticle(int no) throws Exception {
+		boardDAO.delete(no);
+	}
+	@Override
+	 public void uploadFile(UploadFileVO uploadFileVO) {
+	  uploadFileVO.setFileId(boardDAO.selectMaxFileId()+1);
+	  boardDAO.insertFile(uploadFileVO);
+	 }
+	@Override
+	public Object readAttachFile(int no) {
+		return boardDAO.selectAttachFile(no);
+	}
+	@Override
+	public UploadFileVO downloadFile(int fileId) {
+		return boardDAO.selectDownloadFile(fileId);
+	}
+}
